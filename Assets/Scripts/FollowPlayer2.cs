aeeda0dc6f813bf2525e15b67434d21b09925b27
Kwoojinys <aleldna54@gmail.com>
2024-11-08 18:24:13 +0900

    1. 프로젝트 파일 보존용 커밋..

---
+++ Assets/Scripts/FollowPlayer2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer2 : MonoBehaviour
+{
+	public Vector3 offset;			// The offset at which the Health Bar follows the player.
+	
+	private Transform player2;		// Reference to the player.
+	
+	
+	void Awake ()
+	{
+		// Setting up the reference.
+		player2 = GameObject.FindGameObjectWithTag("Player2").transform;
+	}
+	
+	void Update ()
+	{
+		// Set the position to the player's position with the offset.
+		transform.position = player2.position + offset;
+	}
+}
